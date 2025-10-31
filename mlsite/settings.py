eedb5d24987a6f17ec2384a6bd2e6b06c32e2b95
Abaquio <matt.sepulveda00@gmail.com>
2025-10-31 13:51:40 -0300

    actualizacion

--- mlsite/settings.py
+++ mlsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%^cw5r8be*$0=x0w^%u0i!8l6@p5pd7ofb8%osu_tjswts%t@_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matiassepulvedagon.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
