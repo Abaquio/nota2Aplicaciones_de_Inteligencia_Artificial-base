286d459aa537aef5bbea26a1e4a47524504d4998
Abaquio <matt.sepulveda00@gmail.com>
2025-10-31 16:34:53 -0300

    up1

--- mlsite/settings.py
+++ mlsite/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     # agrega aquí tus apps si las tienes (por ejemplo "diabetes", "insurance")
 ]
 
